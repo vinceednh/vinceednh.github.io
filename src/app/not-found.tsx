d1b77b0b53c3ded9bc69062a83d270f1cbe4a2ff
Vincent Dinh <vincent.dinh401@gmail.com>
2025-04-07 19:44:16 -0700

    Update Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <p className="flex h-screen w-screen items-center justify-center text-4xl font-semibold">
+      404 - Page Not Found
+    </p>
+  );
 };
 
 export default NotFound;
